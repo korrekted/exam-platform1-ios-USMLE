208475941bc53795b0daf455f8f6fcfae619415f
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-19 19:18:51 +0300

    copy

---
+++ USMLE/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 250, green: 255, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 73, green: 154, blue: 169)
+    static let mainColorAlpha = UIColor(integralRed: 73, green: 154, blue: 169, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 18, green: 24, blue: 68)
+    
+    static let progress1Color = UIColor(integralRed: 73, green: 154, blue: 169)
+    static let progress2Color = UIColor(integralRed: 18, green: 24, blue: 68)
+    static let progress3Color = UIColor(integralRed: 241, green: 142, blue: 162)
+}
---
+++ USMLE/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams.korrekted.com"
+    static let apiKey = "J9drClgX1WvRiSRF"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "QZezCdTi8x3SvYDw"
+
+    static let applicationTag = "EXAM_USMLE"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appleAppID = "1568062568"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
