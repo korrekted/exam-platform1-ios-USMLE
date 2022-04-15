d5ab54c745c314f4fea9e6b847599727daa2f6d2
akonst17 <akonst17@gmail.com>
2022-04-15 11:01:41 +0300

    upgrade; build 6

--- USMLE/Classes/Misc/Appearance.swift
+++ USMLE/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 73, green: 154, blue: 169)
     static let progress2Color = UIColor(integralRed: 18, green: 24, blue: 68)
     static let progress3Color = UIColor(integralRed: 241, green: 142, blue: 162)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- USMLE/Classes/Service/Stats/Share/StatsShareManager.swift
+++ USMLE/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.usmle.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.usmle.widget")
     }
     
     @discardableResult
